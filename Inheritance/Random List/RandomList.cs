50152c378cb83f64a9cb821cf22a484c307081cf
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-28 10:59:54 +0300

    Add files via upload

---
+++ Inheritance/Random List/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {        
+        public string RandomString() 
+        {
+            var rnd = new Random();
+            int index = rnd.Next(0, this.Count);
+            string str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }
+    }
+}
