befe6c7fd811e2311282974862ee5779dbf22357
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-31 20:35:42 +0300

    Add files via upload

--- Encapsulation/PersonsInfo/Person.cs
+++ Encapsulation/PersonsInfo/Person.cs
@@ -6,22 +6,39 @@ namespace PersonsInfo
 {
     public class Person
     {
-		public string FirstName { get; private set; }
+        public string FirstName { get; private set; }
 
-		public string LastName { get; private set; }
+        public string LastName { get; private set; }
 
-		public int Age { get; private set; }
+        public int Age { get; private set; }
 
-		public Person(string firstName, string lastName, int age)
-		{
-			this.FirstName = firstName;
-			this.LastName = lastName;
-			this.Age = age;
-		}
-		public override string ToString()
-		{
-			return $"{this.FirstName} {this.LastName} is {this.Age} years old.";	
-		}
+        public decimal Salary { get; private set; }
 
-	}
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        
+        public void IncreaseSalary(decimal percentage)
+        {
+            if(this.Age < 30)
+            {
+                this.Salary += this.Salary * percentage / 200; 
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+    }
 }
