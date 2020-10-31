716c485486c14ec8a685d9fd542e08b057798c42
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-10-31 21:20:14 +0300

    Update Person.cs

--- Encapsulation/PersonsInfo/Person.cs
+++ Encapsulation/PersonsInfo/Person.cs
@@ -12,32 +12,16 @@ namespace PersonsInfo
 
         public int Age { get; private set; }
 
-        public decimal Salary { get; private set; }
-
-        public Person(string firstName, string lastName, int age, decimal salary)
+        public Person(string firstName, string lastName, int age)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Age = age;
-            this.Salary = salary;
-        }
-        
-        public void IncreaseSalary(decimal percentage)
-        {
-            if(this.Age < 30)
-            {
-                this.Salary += this.Salary * percentage / 200; 
-            }
-            else
-            {
-                this.Salary += this.Salary * percentage / 100;
-            }
-
         }
 
         public override string ToString()
         {
-            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
         }
 
     }
