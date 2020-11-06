6940d88727ae1f30380d9ec8d55e0dab3ea837fd
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-06 15:26:10 +0300

    Add files via upload

---
+++ Interfaces & Abstraction/BorderControl/Citizen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : Identifiable
+    {
+        private string name;
+
+        private string age;
+
+        private string id;
+
+        public Citizen(string name, string age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+        public string Name { get; private set; }
+        public string Age { get; private set; }
+        public string Id { get; private set; }
+    }
+}
---
+++ Interfaces & Abstraction/BorderControl/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Identifiable> society = new List<Identifiable>();
+            
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] info = input.Split();
+                if(info.Length == 2)
+                {
+                    Identifiable robot = new Robot(info[0], info[1]);
+                    society.Add(robot);
+                }
+                else if(info.Length == 3)
+                {
+                    Identifiable citizen = new Citizen(info[0], info[1], info[2]);
+                    society.Add(citizen);
+                }
+            }
+            string fakeID = Console.ReadLine();
+            List<Identifiable> detained = society.Where(i => i.Id.EndsWith(fakeID)).ToList();
+            foreach (var element in detained)
+            {
+                Console.WriteLine(element.Id);
+            }
+        }
+    }
+}
