1ad209be856c75ad3373d2418c337d93b8f0da97
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-10 10:53:16 +0300

    Add files via upload

---
+++ Polimorphism/Vehicles Extension/Models/Bus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Bus : Vehicle
+    {
+        private const double Airconditioning = 1.4;
+
+        public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+        }
+
+        public override double FuelConsumption
+        {
+            get
+            {
+                return base.FuelConsumption;
+            }
+
+            protected set
+            {
+                base.FuelConsumption = value + Airconditioning;
+            }
+        }
+
+        public override double FuelConsumptionEmpty
+        {
+            get
+            {
+                return base.FuelConsumption;
+            }
+
+            protected set
+            {
+                base.FuelConsumption = value;
+            }
+        }
+    }
+}
---
+++ Polimorphism/Vehicles Extension/Models/Truck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double Airconditioning = 1.6;
+        private const double TankLeak = 0.95;
+        
+        public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) 
+            : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+        }
+
+        public override double FuelConsumption
+        {
+            get
+            {
+                return base.FuelConsumption;
+            }
+
+            protected set
+            {
+                base.FuelConsumption = value + Airconditioning;
+            }
+        }
+
+        //public override double FuelQuantity => base.FuelQuantity * TankLeak;
+
+        public override void Refuel(double fuelAmount)
+        {
+            base.Refuel(fuelAmount);
+            this.FuelQuantity -= fuelAmount * TankLeak;
+        }
+    }
+}
