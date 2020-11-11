7a7439de541c20041e0f37b2385b1a99950f82d9
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-11 10:14:11 +0300

    Add files via upload

---
+++ Polimorphism/Raiding/Core/Engine.cs
@@ -0,0 +1,63 @@
+﻿using Raiding.Common;
+using Raiding.Core.Contracts;
+using Raiding.Factory;
+using Raiding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raiding.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICollection<BaseHero> raidGroup;
+
+        private HeroFactory heroFactory;
+
+        public Engine()
+        {
+            this.raidGroup = new List<BaseHero>();
+            this.heroFactory = new HeroFactory();
+        }
+        public void Run()
+        {
+            int n = int.Parse(Console.ReadLine());
+            while (n > this.raidGroup.Count)
+            {
+                
+                BaseHero currentHero = null;
+                try
+                {
+                    string heroName = Console.ReadLine();
+                    string heroType = Console.ReadLine();
+                    currentHero = this.heroFactory.ProduceHero(heroType, heroName);
+                    this.raidGroup.Add(currentHero);
+                }
+                catch (ArgumentException em)
+                {
+                    Console.WriteLine(em.Message);
+                    continue;
+                }
+
+            }
+
+            int bossPower = int.Parse(Console.ReadLine());
+            int heroesTotalPower = 0;
+
+            if (this.raidGroup.Count != 0)
+            {
+                foreach (var bhero in raidGroup)
+                {
+                    Console.WriteLine(bhero.CastAbility());
+                }
+                heroesTotalPower = raidGroup.Sum(h => h.Power);
+            }
+
+
+            Console.WriteLine((heroesTotalPower >= bossPower) ?
+            "Victory!" : "Defeat...");
+
+        }
+    }
+}
---
+++ Polimorphism/Raiding/Factory/HeroFactory.cs
@@ -0,0 +1,38 @@
+﻿using Raiding.Common;
+using Raiding.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding.Factory
+{
+    public class HeroFactory
+    {
+        public BaseHero ProduceHero(string type, string name)
+        {
+            BaseHero bhero = null;
+            if (type == "Druid")
+            {
+                bhero = new Druid(name);
+            }
+            else if (type == "Paladin")
+            {
+                bhero = new Paladin(name);
+            }
+            else if (type == "Rogue")
+            {
+                bhero = new Rogue(name);
+            }
+            else if (type == "Warrior")
+            {
+                bhero = new Warrior(name);
+            }
+
+            if (bhero == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidHeroExcMsg);
+            }
+            return bhero;
+        }
+    }
+}
