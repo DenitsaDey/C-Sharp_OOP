c6529cfaee7955aaebf33ff616d6ac1b60a42d0d
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-13 08:35:35 +0300

    Add files via upload

---
+++ Exception Handling/05. Convert.ToDouble/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _05._Convert.ToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = string.Empty;
+            while ((num = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    double n = Convert.ToDouble(num);
+                    Console.WriteLine(n);
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                }
+                catch (OverflowException ofe)
+                {
+                    Console.WriteLine(ofe.Message);
+                }
+            }
+        }
+        
+    }
+}
---
+++ Exception Handling/06. Valid Person/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Valid_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person = new Person("Ivan", "Petrov", 24);
+                Person noName = new Person(string.Empty, "Goshev", 31);
+                Person noLastName = new Person("Ivan", null, 63);
+                Person negativeAge = new Person("Stoyan", "Kolev", -1);
+                Person tooOldForThisProgram = new Person("Kiril", "Todorov", 121);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            catch (ArgumentOutOfRangeException aor)
+            {
+                Console.WriteLine("Exception thrown: {0}", aor.Message);
+            }
+        }
+    }
+}
