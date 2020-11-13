9937e6c4eee1847799f54da350d8be099751b14a
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-13 08:29:26 +0300

    Add files via upload

---
+++ Exception Handling/01. Square Root/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            try
+            {
+                if (n < 0)
+                {
+                    throw new InvalidOperationException();
+                }
+                Console.WriteLine(Math.Pow(n, 2));
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
