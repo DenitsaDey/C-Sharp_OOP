a9c65e1a2dc86d47d38695e396312f8c75072c3f
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-13 08:33:08 +0300

    Add files via upload

---
+++ Exception Handling/02. Enter Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _02._Enter_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                string n = Console.ReadLine();
+                try
+                {
+                    ReadNumber(n, start, end);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    i = 0;
+                    Console.WriteLine("Start again!");
+                }
+                catch (FormatException fe)
+                {
+                    i = 0;
+                    Console.WriteLine(fe.Message);
+                }
+            }
+        }
+
+        private static int ReadNumber(string num, int start, int end)
+        {
+            int n = 0;
+
+            try
+            {
+                n = int.Parse(num);
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Entry is not an integer");
+            }
+
+            if (start >= n || n >= end)
+            {
+                throw new ArgumentOutOfRangeException("Number was out of specified range!");
+            }
+            return n;
+        }
+    }
+}
---
+++ Exception Handling/03. Fixing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03._Fixing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[5]
+            {
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+            };
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekdays[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ior)
+            {
+                Console.WriteLine(ior.Message);
+            }
+        }
+    }
+}
