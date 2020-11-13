a0de46c27e97d2bb43ae26f723e03628b520a724
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-13 13:01:46 +0300

    Add files via upload

---
+++ Exception Handling/07. Custom Exception/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _06._Valid_Person
+{
+    public class Student : Person
+    {
+        public Student(string firstName, string email)
+            : base(firstName)
+        {
+            this.Email = email;
+        }
+
+        public string Email { get; private set; }
+
+        public override string FirstName
+        {
+            get => base.FirstName;
+            set
+            {
+                if (!value.All(c => char.IsLetter(c)))
+                {
+                    throw new InvalidPersonNameException();
+                }
+                base.FirstName = value;
+            }
+        }
+    }
+}
