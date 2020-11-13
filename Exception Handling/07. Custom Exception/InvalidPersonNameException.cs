4a72d6f762b355547bfd459c78f1bf2bc4c2e81b
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-13 13:00:47 +0300

    Add files via upload

---
+++ Exception Handling/07. Custom Exception/InvalidPersonNameException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._Valid_Person
+{
+    public class InvalidPersonNameException : Exception
+    {
+        private const string InvalidPersonExcMsg = "Name should contain only letters.";
+
+        public InvalidPersonNameException()
+            : base(InvalidPersonExcMsg)
+        {
+
+        }
+
+        public InvalidPersonNameException(string message)
+            :base (message)
+        {
+
+        }
+    }
+}
---
+++ Exception Handling/07. Custom Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Valid_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person person = new Student("Gin4o", "email");
+                
+            }
+            catch(InvalidPersonNameException ipn)
+            {
+                Console.WriteLine("Exception thrown: {0}", ipn.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            catch (ArgumentOutOfRangeException aor)
+            {
+                Console.WriteLine("Exception thrown: {0}", aor.Message);
+            }
+        }
+    }
+}
