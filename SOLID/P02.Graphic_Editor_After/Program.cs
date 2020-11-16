af5fe1847858e6761751fe1dd23b42178ad0151c
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-16 10:05:19 +0300

    Add files via upload

---
+++ SOLID/P02.Graphic_Editor_After/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02.Graphic_Editor_After
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
