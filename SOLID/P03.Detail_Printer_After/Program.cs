d600a517eb6a9a978a4b1fe920a1bc258809e14a
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-16 10:08:24 +0300

    Add files via upload

---
+++ SOLID/P03.Detail_Printer_After/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03.Detail_Printer_After
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SOLID/P04.Recharge_After/Contracts/ISleeper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.Recharge_After.Contracts
+{
+    interface ISleeper
+    {
+    }
+}
