4d7e277661063b89642e3ebb02a55d961793448e
Deni Dey <72288133+DenitsaDey@users.noreply.github.com>
2020-11-19 12:02:32 +0300

    Add files via upload

---
+++ Reflection and Attributes/ValidationAttributes/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class Person
+    {
+        public Person(string fullName, int Age)
+        {
+            this.FullName = fullName;
+            this.Age = Age;
+        }
+
+        [MyRequired]
+        public string FullName { get; set; }
+
+        [MyRange(18, 65)]
+        public int Age { get; set; }
+    }
+}
